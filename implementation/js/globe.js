6e2ef10d5a8cbd93b4b4cb28cb9b56b98040ad36
VictoriaLee <victoriaalhl@gmail.com>
2016-10-11 08:09:20 +1000

    resized globe

--- implementation/js/globe.js
+++ implementation/js/globe.js
@@ -5,10 +5,10 @@
 
 function initialize() {
         var options = {
-          sky: true,
-          atmosphere: true,
+          sky: false,
+          atmosphere: false,
           dragging: true,
-          tilting: true,
+          tilting: false,
           zooming: true,
           center: [46.8011, 8.2266],
           zoom: 2,
@@ -20,6 +20,16 @@ function initialize() {
           tms: true
         });
         natural.addTo(earth);
+
+        var marker = WE.marker([51.5, -0.09]).addTo(earth);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+
+        var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
+        marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
+
+        var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+
+        //
 
         var toner = WE.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
           attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.',
@@ -36,5 +46,3 @@ function addSomeMarkers() {
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
 
              };
-
-
